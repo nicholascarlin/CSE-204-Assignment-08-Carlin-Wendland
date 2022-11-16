72702abddf7187c81bca7356a11088133069552a
nicholascarlin <54487246+nicholascarlin@users.noreply.github.com>
2022-11-16 13:23:21 -0600

    Most API Functionality

--- scripts.js
+++ scripts.js
@@ -1,13 +1,20 @@
 let api_key = '612728-881111-6debe3-c26de7-2b9424';
 
-const TestFunction = () => {
-	console.log('Test');
+const HandlePost = () => {
+	let inputRef = document.getElementById('todo-input');
+	console.log(inputRef.value);
+
+	if (inputRef.value !== ' ') {
+		console.log('called');
+		PostToDo(inputRef.value);
+		inputRef.value = '';
+	}
 };
 
-const PostToDo = () => {
+const PostToDo = (todoText) => {
 	// Setting variable for form input (get from HTML form)
 	var data = {
-		text: 'Some Text',
+		text: todoText,
 	};
 
 	// Initalize AJAX Request
@@ -18,9 +25,7 @@ const PostToDo = () => {
 		// Wait for readyState = 4 & 200 response
 		if (this.readyState == 4 && this.status == 200) {
 			// parse JSON response
-			var todo = JSON.parse(this.responseText);
-
-			console.log(todo);
+			FetchToDos();
 		} else if (this.readyState == 4) {
 			// this.status !== 200, error from server
 			console.log(this.responseText);
@@ -30,6 +35,57 @@ const PostToDo = () => {
 	xhttp2.open('POST', 'https://cse204.work/todos', true);
 
 	xhttp2.setRequestHeader('Content-type', 'application/json');
-	xhttp2.setRequestHeader('x-api-key', 'abc123');
+	xhttp2.setRequestHeader('x-api-key', api_key);
 	xhttp2.send(JSON.stringify(data));
+};
+
+const FetchToDos = async () => {
+	var xhttp = new XMLHttpRequest();
+
+	xhttp.onreadystatechange = function () {
+		if (this.readyState == 4 && this.status == 200) {
+			var todos = JSON.parse(this.responseText);
+			console.log(todos);
+			UpdateHTML(todos);
+		}
+	};
+
+	xhttp.open('GET', 'https://cse204.work/todos', true);
+	xhttp.setRequestHeader('x-api-key', api_key);
+	xhttp.send();
+};
+
+const UpdateHTML = (newList) => {
+	let listItem = document.getElementById('todo-list');
+	listItem.innerHTML = '';
+	newList.map((item) => {
+		console.log(item);
+		var tempItem = document.createElement('li');
+		tempItem.id = item.id;
+		tempItem.classList.add('todo-item');
+		tempItem.addEventListener('click', function handleClick(event) {
+			HandleDelete(event);
+		});
+		var textNode = document.createTextNode(item.text);
+		tempItem.appendChild(textNode);
+		listItem.appendChild(tempItem);
+	});
+};
+
+const HandleDelete = (event) => {
+	console.log('CLICKED DELTE', event.target.id);
+	const url = `https://cse204.work/todos/${event.target.id}`;
+	console.log('URL', url);
+
+	var xhttp = new XMLHttpRequest();
+
+	xhttp.onreadystatechange = function () {
+		if (this.readyState == 4 && this.status == 200) {
+			FetchToDos();
+		}
+	};
+
+	xhttp.open('DELETE', url, true);
+	xhttp.setRequestHeader('x-api-key', api_key);
+	xhttp.send();
 };
